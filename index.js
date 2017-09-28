6395411ead7976db363d56f8610475490ab46b84
SavyParchman <savyparchman@gmail.com>
2017-09-28 14:35:23 -0500

    Finished.

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require('./strategy');
+const request = require('request');
 
 const app = express();
+
 app.use( session({
-  secret: '@nyth!ng y0u w@nT',
+  secret: 'batman',
   resave: false,
   saveUninitialized: false
 }));
 
+app.use( passport.initialize() );
+app.use( passport.session() );
+passport.use( strategy );
+
+
+//<!--=====---- ENDPOINTS ----=======-->
+app.get('/login', passport.authenticate('auth0',
+  { successRedirect: '/followers', 
+    failureRedirect: '/login',
+    failureFlash: true,
+    connection: 'github'
+  }));
+
+app.get('/followers', ( req, res, next ) => {
+  if ( req.user ) {
+    const FollowersRequest = {
+      url: req.user.followers,
+      headers: { 'User-Agent': req.user.clientID }};
+  request(FollowersRequest, ( error, response, body ) => {
+    res.status(200).send(body); });
+    } else { res.redirect('/login'); }
+});
+
+
+//<!--=====---- SERIALIZE / DESERIALIZE ----=======-->
+passport.serializeUser( (user, done) => {
+  const { _json } = user;
+  done(null, { 
+    clientID: _json.clientID,
+    email: _json.email,
+    name: _json.name,
+    followers: _json.followers_url
+  });
+});
+
+passport.deserializeUser( (obj, done) => {
+  done(null, obj);
+});
+   
+
+//<!--=====---- LISTEN ----=======-->
 const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.listen( port, () => { 
+  console.log(`Magic Happening on Port ${port}`); 
+});
---
+++ strategy.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const { domain, clientID, clientSecret } = require('./config');
+
+
+module.exports = new Auth0Strategy( {
+  domain: domain,
+  clientID: clientID,
+  clientSecret: clientSecret,
+  callbackURL: '/login'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+  return done(null, profile);
+});
+
